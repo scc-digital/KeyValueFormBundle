dda19d6b9cd50abb28243b1cdef49822acbc22cf
mathieu <mathieu.lagarde@sccd.lu>
2023-09-21 17:21:43 +0200

    fix: use sonata type rather than SF one

--- Form/Type/KeyValueType.php
+++ Form/Type/KeyValueType.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Scc\KeyValueFormBundle\Form\Type;
 
 use Scc\KeyValueFormBundle\Form\DataTransformer\HashToKeyValueArrayTransformer;
+use Sonata\AdminBundle\Form\Type\CollectionType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
