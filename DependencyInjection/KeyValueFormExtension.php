cd6c89d3d1d741d16bef439d3804536c2550ed69
mathieu <mathieu.lagarde@sccd.lu>
2023-09-21 17:04:13 +0200

    fix: remove config loading

--- DependencyInjection/KeyValueFormExtension.php
+++ DependencyInjection/KeyValueFormExtension.php
@@ -14,9 +14,6 @@ class KeyValueFormExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }
