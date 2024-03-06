9f1aaaafdbf40cefb0814d1a65e96e65ede96493
mathieu <mathieu.lagarde@sccd.lu>
2024-03-06 11:25:23 +0100

    fix: DataTransformerInterface implementation

--- Form/DataTransformer/HashToKeyValueArrayTransformer.php
+++ Form/DataTransformer/HashToKeyValueArrayTransformer.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\Exception\TransformationFailedException;
 
 class HashToKeyValueArrayTransformer implements DataTransformerInterface
 {
-    public function transform($value)
+    public function transform($value): mixed
     {
         return $value;
     }
