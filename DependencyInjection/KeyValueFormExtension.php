3a834d4f94fbd0b5be6a7252e3af2671a144f425
mathieu <mathieu.lagarde@sccd.lu>
2025-08-01 10:57:02 +0200

    feat: update conf loader

--- DependencyInjection/KeyValueFormExtension.php
+++ DependencyInjection/KeyValueFormExtension.php
@@ -5,7 +5,7 @@ namespace Scc\KeyValueFormBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class KeyValueFormExtension extends Extension
 {
@@ -14,7 +14,7 @@ class KeyValueFormExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
     }
 }
